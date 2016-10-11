e764e3a8ba6f4bc941d1497b658b59998c4ebf9c
Bun suwanparsert <bun.swp@gmail.com>
2016-10-11 11:41:16 +0700

    Update rfi_lang.php

--- rfi_lang.php
+++ rfi_lang.php
@@ -1,9 +1,5 @@
 <?php 
   echo "EN Land Edited"; 
   show_source('en_lang.php');
-
-  $myfile = fopen("en_lang.php", "w") or die("Unable to open file!");
-  $txt = "<?php show_source('index.php'); ?>";
-  fwrite($myfile, $txt);
-  fclose($myfile);
+  show_source('fr_lang.php');
 ?>
